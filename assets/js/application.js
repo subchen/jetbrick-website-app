e03e11c63706020fd5923179903ed372944958ab
Guoqiang Chen <subchen@gmail.com>
2014-10-30 04:50:40 +0000

    copy source code support

--- assets/js/application.js
+++ assets/js/application.js
@@ -50,3 +50,30 @@
 })(typeof window !== "undefined" ? window : this, jQuery);
 
 
+$(function() {
+    $("pre > code.highlight").each(function() {
+        var $copy = '<div class="zero-clipboard"><span class="btn-clipboard">Copy</span></div>';
+        $(this).parent().before($copy);
+    });
+    
+    var clip = new ZeroClipboard($(".btn-clipboard"));
+    var $bridge = $("#global-zeroclipboard-html-bridge");
+ 
+    clip.on("ready", function(e) {
+        $bridge.data("placement", "left").attr("title", "Copy to clipboard").tooltip();
+    });
+ 
+    clip.on("copy", function(e) {
+        var c = $(e.target).parent().next();
+        clip.setText(c.text());
+    });
+ 
+    clip.on("aftercopy", function(e) {
+        $bridge.attr("title", "Copied!").tooltip("fixTitle").tooltip("show").attr("title", "Copy to clipboard").tooltip("fixTitle");
+    });
+ 
+    clip.on("error", function(e) {
+        $bridge.attr("title", e.message).tooltip("fixTitle").tooltip("show");
+    });
+});
+
